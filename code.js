9f67cf210c4d59ba7a31cd8a1a1ee699c5e06eec
George <georgecrane@mac.com>
2020-12-24 10:20:16 -0500

    Updates and additions 12/24/20

--- code.js
+++ code.js
@@ -107,8 +107,15 @@ function filter_showvals () {
         $('div.summary-item').css('display','none');
     }
 
+    // make sure ids only has unique values
+    var t = [];
+    for(var x = 0; x < ids.length; x++){
+        if(t.indexOf(ids[x]) == -1) {t.push(ids[x]);}
+    }
+    ids = t;
+
     $('div.summary-item').each(function(index, value) {
-        xidsx = [...ids]; // copy the array of checked items
+        xidsx = ids; // copy the array of checked items
         $(this).find('div.summary-content div.summary-metadata-container div.summary-metadata span.summary-metadata-item--cats a').filter(function (index2) {
             var t = this.href.indexOf('?category=');
             var i = xidsx.indexOf(this.href.substr(t+10));
---
+++ gallery.js
@@ -0,0 +1,80 @@
+/* ------------------------------------------------------------------- */
+/* Slideshow gallery                                                   */
+/* ------------------------------------------------------------------- */
+
+
+function doGalleryShow() {
+
+    /* Don't do the fancy gallery if we are in IE 11 or earlier */
+    /*
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE '); // IE 10 and older
+    var trident = ua.indexOf('Trident/'); // IE 11
+    if (msie > 0 || trident > 0) {
+        var gallery = $('#page  article:first-child section.gallery-section').first().find('figure');
+        gallery.closest('section').css('display', 'none'); 
+        return;
+    }
+    */
+    // get some selectors and data 
+    var background = $('#page article:first-child section:first-child div.section-background');
+    var gallery = $('#page  article:first-child section.gallery-section').first().find('figure');
+
+    // If no gallery found so abort
+    if (gallery.length == 0) {
+        return false;
+    } 
+
+    // Hide the initial template, not needed.
+    background.find('img').css('display','none');
+
+    // https://stackoverflow.com/questions/326069/how-to-identify-if-a-webpage-is-being-loaded-inside-an-iframe-or-directly-into-t
+    // See if we are editing the SquareSpace page, if so hide the gallery section
+    var isEditor = window.frameElement ? true : false;
+    if (isEditor == false) {
+       gallery.closest('section').css('display', 'none'); 
+    }
+
+    // Loop through each figure and add to the list of slides 
+    gallery.each(function() {
+
+        var imgtemp = $(this).find('img');
+        var imgcap = $(this).find('figcaption p.gallery-caption-content').text();
+        var caplink = $(this).find('a').first().attr('href');
+        if (caplink) {
+            imgcap = '<a href="' + caplink + '">' + imgcap + '</a>';
+        }
+        if (imgcap) { imgcap = '<div class="slideCaption">' + imgcap + '</div>';}
+        var imgpos = imgtemp.attr('data-image-focal-point');
+
+        imgpos = imgpos.split(",");
+        var temp = "";
+        for (var i = 0; i < imgpos.length; i++) {
+            imgpos[i] = imgpos[i] * 100;
+            temp = temp + " " + imgpos[i] + "%";
+        }
+        imgpos = temp.trim();
+        var style = ' style="object-position:' + imgpos + ';';
+        temp = '<div class="mySlides"><img src="' + imgtemp.attr('data-src') + '"' + style + '">' +
+        imgcap + '</div>';
+        background.append(temp);
+    });
+
+    // start the slideshow
+    
+    carousel();
+}
+  
+  var myIndex = 0;
+  function carousel() {
+    var i;
+    var background = $('#page article:first-child section:first-child div.section-background');
+    var x = background.find('.mySlides');
+    if (myIndex >= x.length) {
+      myIndex = 0
+    }
+    x.removeClass("opaque");
+    background.find('div.mySlides').eq(myIndex).addClass("opaque");
+    myIndex++;
+    setTimeout(carousel, 8000);
+  }
