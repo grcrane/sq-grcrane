c41c590b783f125931a525a88d8dad16fe993304
George <georgecrane@mac.com>
2021-02-27 10:48:51 -0500

    Update faqs.js

--- faqs.js
+++ faqs.js
@@ -31,9 +31,10 @@ function do_faqs(file_id, tabs) {
     var sheet = 'FAQS';
     var url = 'https://docs.google.com/spreadsheets/u/0/d/'
     + file_id + '/gviz/tq?sheet=' + sheet + '&tqx=out:json&headers=1&tq=' + 
-    escape("SELECT A, B, C, D WHERE B != 'Yes");
+    escape("SELECT A, B, C, D WHERE B = 'No'");
     var spreadSheetLink = 'https://docs.google.com/spreadsheets/d/' + file_id + '/edit';
     var faqlist = get_spreadsheet(url);
+    console.log(faqlist);
     // Test for some error conditions 
     if(faqlist.length == 0) {
         $('.faq_container').append('<br>Ooops.. unable to read spreadsheet</br>');
@@ -51,22 +52,24 @@ function do_faqs(file_id, tabs) {
     tabs.forEach(function(item, key) {
      
       var val = item[0];
+      // camelcase css tag for background
+      var background = 'color' + item[0].charAt(0).toUpperCase() + item[0].slice(1).toLowerCase();
       if (item.length>1) { val = item[1];}
-      tabLinks = tabLinks + '<li><a href="#tabs-' + +(key+1) + '">' + val + '</a></li>\n'; 
+      tabLinks = tabLinks + '<li class="' + background + '"><a href="#tabs-' + +(key+1) + '">' + val + '</a></li>\n'; 
       var lookfor = item[0].toLowerCase();
       
       // Loop for the questions/answers in each tab 
       out = out + '<div id="tabs-' + +(key+1) + '">\n<div class="accordian">\n'; 
       faqs.forEach(function(item2, key2) {
         if (item2.c[listCol] != null && item2.c[listCol].v.toLowerCase() == lookfor) {
-          out = out + '<h3>' + item2.c[questionCol].v + '</h3><div>\n';
+          out = out + '<h3 class="' + background + '">' + item2.c[questionCol].v + '</h3><div>\n';
           out = out + '<p>' + item2.c[answerCol].v + '</p></div>\n';
         }
       });
       out = out + '</div>\n</div>\n';
 
     });
-    // pull everything together
+    // use tabs if we have more than one 
     if (tabs.length > 1) {
       out = '<div id="tabs"><ul>' + tabLinks + '</ul>' + out + '</div></div>\n';
     }
