b6cf7903dbb2ee1466032cf56d61cdb2e16279e8
George <georgecrane@mac.com>
2021-02-27 10:41:07 -0500

    Initial faqs

---
+++ faqs.js
@@ -0,0 +1,75 @@
+/* ------------------------------------------------------------------- */
+/* FAQs                                                                */
+/* ------------------------------------------------------------------- */
+
+function get_spreadsheet(theurl) {
+  var result = "";
+  $.ajax({
+      url: theurl,
+      dataType: 'text',
+      async: false,
+      success: function(data) {
+          i = data.indexOf('(');
+          j = data.lastIndexOf(')');
+          data = data.substr(i + 1, j - 1 - i);
+          var data = JSON.parse(data);
+          result = data;
+      }
+  });
+  return result;
+}
+
+function do_faqs(file_id, tabs) {
+
+    var listCol = 0;
+    var hideCol = 1;
+    var questionCol = 2;
+    var answerCol = 3;
+    //var theList = lists.split(','); 
+
+    // Get the spreadsheet data
+    var sheet = 'FAQS';
+    var url = 'https://docs.google.com/spreadsheets/u/0/d/'
+    + file_id + '/gviz/tq?sheet=' + sheet + '&tqx=out:json&headers=1&tq=' + 
+    escape("SELECT A, B, C, D WHERE B != 'Yes");
+    var spreadSheetLink = 'https://docs.google.com/spreadsheets/d/' + file_id + '/edit';
+    var faqlist = get_spreadsheet(url);
+    // Test for some error conditions 
+    if(faqlist.length == 0) {
+        $('.faq_container').append('<br>Ooops.. unable to read spreadsheet</br>');
+        return;
+    }
+    if(tabs.length == 0) {
+        $('.faq_container').append('<br>Ooops.. No lists name provided</br>');
+        return;
+    }
+
+    // Loop for each tab 
+    var tabLinks = ''; 
+    var out = '';
+    var faqs = faqlist.table.rows;
+    tabs.forEach(function(item, key) {
+     
+      var val = item[0];
+      if (item.length>1) { val = item[1];}
+      tabLinks = tabLinks + '<li><a href="#tabs-' + +(key+1) + '">' + val + '</a></li>\n'; 
+      var lookfor = item[0].toLowerCase();
+      
+      // Loop for the questions/answers in each tab 
+      out = out + '<div id="tabs-' + +(key+1) + '">\n<div class="accordian">\n'; 
+      faqs.forEach(function(item2, key2) {
+        if (item2.c[listCol] != null && item2.c[listCol].v.toLowerCase() == lookfor) {
+          out = out + '<h3>' + item2.c[questionCol].v + '</h3><div>\n';
+          out = out + '<p>' + item2.c[answerCol].v + '</p></div>\n';
+        }
+      });
+      out = out + '</div>\n</div>\n';
+
+    });
+    // pull everything together
+    if (tabs.length > 1) {
+      out = '<div id="tabs"><ul>' + tabLinks + '</ul>' + out + '</div></div>\n';
+    }
+
+    $(out).appendTo('.faq_container');
+}
